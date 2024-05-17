b97c1409ba729220f1bb0293864371a84a8b8552
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2024-05-17 13:45:42 +0300

    intialize dynamic links service

---
+++ lib/services/dynamic_link.service.dart
@@ -0,0 +1,18 @@
+library alghwalbi_core;
+
+import 'package:share_plus/share_plus.dart';
+
+class DynamicLinksServices {
+  static Future shareLink(String link) async {
+    return Share.share(link);
+  }
+
+  static Future<String> generateShortLinkList(
+      String title, String message, String type, String id) async {
+    return "";
+  }
+
+  static Future<String> retrieveDynamicLink() async {
+    return "";
+  }
+}
