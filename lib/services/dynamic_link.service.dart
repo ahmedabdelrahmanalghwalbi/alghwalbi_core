404f0c444ee8a1f78e839001b244c199738e20ea
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2024-05-19 18:49:19 +0300

    intializing-seoservice-dynamic-link service

--- lib/services/dynamic_link.service.dart
+++ lib/services/dynamic_link.service.dart
@@ -2,6 +2,7 @@ library alghwalbi_core;
 
 import 'package:share_plus/share_plus.dart';
 
+//TODO: add DYNAMIC LINK Service
 class DynamicLinksServices {
   static Future shareLink(String link) async {
     return Share.share(link);
---
+++ lib/services/seo.service.dart
@@ -0,0 +1,2 @@
+//TODO: add SEO SERVICE.
+class SeoService {}
